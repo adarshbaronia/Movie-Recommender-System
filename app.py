7a5a173fb7f2d83b0c4d06f35c4312e7da756346
Adarsh Raj Baronia <37274176+adarshbaronia@users.noreply.github.com>
2021-07-10 15:49:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,56 @@
 from flask import Flask, render_template, request
 #import requests
 import Movie_Recommender
-from Movie_Recommender import doc_sim_df, movies_list
-import pandas as pd
+#from Movie_Recommender import doc_sim_df, movies_list
 import numpy as np
+
+
+import nltk
+#nltk.download('punkt')
+nltk.download('stopwords')
+import pandas as pd
+df=pd.read_csv('tmdb_5000_movies.csv')
+df.describe()
+
+df=df[['title','tagline','overview','popularity']]
+df.tagline = df.tagline.fillna('',inplace=True)
+#df.dropna(inplace=True)
+df['description']=df['tagline'].map(str)+' '+df['overview']
+df=df.sort_values(by='popularity', ascending=False)
+
+import re
+import numpy as np
+stop_words=nltk.corpus.stopwords.words('english')
+
+def normalize_document(doc):
+    # lower case and remove special characters\whitespaces
+    doc = re.sub(r'[^a-zA-Z0-9\s]', '', str(doc), re.I|re.A)
+    doc = doc.lower()
+    doc = doc.strip()
+    # tokenize document
+    tokens = nltk.word_tokenize(doc)
+    #filter stopwords out of document
+    filtered_tokens = [token for token in tokens if token not in stop_words]
+    # re-create document from filtered tokens
+    doc = ' '.join(filtered_tokens)
+    return doc
+
+normalize_corpus=np.vectorize(normalize_document)
+norm_corpus=normalize_corpus(list(df['description']))
+len(norm_corpus)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+tf = TfidfVectorizer(ngram_range=(1,2), min_df=2)
+tfidf_matrix = tf.fit_transform(norm_corpus)
+
+#compute pairwise document similarity
+from sklearn.metrics.pairwise import cosine_similarity
+doc_sim=cosine_similarity(tfidf_matrix)
+doc_sim_df=pd.DataFrame(doc_sim)
+#doc_sim_df.head()
+#building a movie recommender function to recommend top 5 similar movies
+#movie title, movies title list and document similarities matrix will be given as imput
+movies_list=df['title'].values
 
 app = Flask(__name__)
 
